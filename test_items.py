223b42414f366d104994afa8aa48d85afd935ba4
sprilukov <123167086+sprilukov@users.noreply.github.com>
2023-02-03 12:34:09 +0300

    Add files via upload

--- test_items.py
+++ test_items.py
@@ -7,9 +7,9 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 class TestMainPage1():
 
-    def test_guest_should_see_login_link(self, browser):
+    def test_languages(self, browser):
         browser.get(link)
-        time.sleep(4)
-        button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
-        button.click()
+        time.sleep(30)
+        x = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
+        assert x , 'Кнопка не найдена!'
         time.sleep(4)
